93ca64c960495a53331ece7e1685c87c8761c132
Sergio1226 <tu_correo@example.com>
2026-08-27 15:42:52 -0500

    count nulls

--- main.py
+++ main.py
@@ -3,8 +3,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+#1. Carga de datos
 df = pd.read_csv("heart_disease.csv")
 
+#info
 print(f"Dimensiones del dataset: {df.shape[0]} filas x {df.shape[1]} columnas")
-
 print(df.head())
+# print("Info \n")
+# print(df.info)
+
+#2 Analisis Exploratorio y Destribucion
+#nulos representados por ? 
+print("\n-- Conteno de nulos y Tipos")
+#print(df.isnull().sum().sum(),"valores nulos detectados.")
+print("Nulos  '?' detectados:", (df == '?').sum().sum())
+
+#Balance de la variable objetivo [Target]
+#target_counts=df['target'].value_counts(normalize=True) * 100
+
+
+
