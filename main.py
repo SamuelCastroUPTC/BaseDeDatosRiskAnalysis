b2897748d6f12f5a87a682f3d8005368d3755866
Sergio1226 <tu_correo@example.com>
2026-08-27 15:25:25 -0500

    load csv

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("samuel es gay");
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("heart_disease.csv")
+
+print(f"Dimensiones del dataset: {df.shape[0]} filas x {df.shape[1]} columnas")
+
+print(df.head())
